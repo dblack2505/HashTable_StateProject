80e2024e69b9cc02a538dff46d8404fe11088a6f
Dalton Black <Dalton Black@Dalton-Dell>
2021-06-07 15:09:37 -0400

    Classes

--- HashTable_StateProject/src/HashTable.java
+++ HashTable_StateProject/src/HashTable.java
@@ -1,4 +1,366 @@
+import HashTable.Link;
+import HashTable.Node;
 
+/**
+ * Class HashTable
+ * 
+ * @author Dalton Black
+ * @version 4/13/2021
+ */
 public class HashTable {
 
+	private Link[] hashArray;
+	private int arraySize;
+	private static HashTable[] a;
+	private static int nElems;
+	private static int max = 50;
+	private String state;
+	private long population;
+	private long covidDeath;
+
+	/**
+	 * Class Node
+	 * 
+	 * @author Dalton Black
+	 * @version 04/13/2021
+	 */
+	private class Node {
+
+		String name;
+		long population;
+		long deaths;
+		Node nextNode;
+
+		/**
+		 * Constructor to set these variables.
+		 * @param name - String state name
+		 * @param population - long population
+		 * @param deaths - long deaths
+		 */
+		public Node(String name, long population, long deaths) {
+			this.name = name;
+			this.population = population;
+			this.deaths = deaths;
+		}
+
+		/**
+		 * Method to print a state and it's death rate.
+		 */
+		public void printNode() {
+			System.out.printf("%-30s %-20.2f\n", name, ((double) deaths/population) * 100000);
+		}
+
+	}
+
+	/**
+	 * Class Link
+	 * 
+	 * @author Dalton Black
+	 * @version 04/13/2021
+	 */
+	private class Link {
+		private Node first;
+		private Node last;
+		private String state;
+		private long population;
+		private long deaths;
+
+		/**
+		 * Constructor.
+		 */
+		public Link() {}
+
+		/**
+		 * Method to insert a new state into Linked List.
+		 * @param newNode - Node newNode to be inserted
+		 */
+		public void insert(Node newNode) {
+			if(isEmpty())
+				first = newNode;
+			else 
+				last.nextNode = newNode;
+			last = newNode;
+		}
+
+		/**
+		 * Method checks to see if the Linked List is empty.
+		 * @return boolean
+		 */
+		private boolean isEmpty() {
+			return first == null;
+		}
+
+		/**
+		 * Method the display the Linked List by calling printNode in Node class.
+		 */
+		public void displayList() {
+			Node current = first;
+			int counter = 0;
+			while(current != null) {
+				if(counter > 0) 
+					System.out.print("    ");
+				current.printNode();
+				current = current.nextNode;
+				counter++;
+			}
+			System.out.println("");
+		}
+
+		/**
+		 * Method deletes a link from the Linked List
+		 * @param state - String state
+		 */
+		public void delete(String state) {
+			Node previous = null;
+			Node current = first;
+			while(current != null && state.compareTo(current.name) > 0) {
+				previous = current;
+				current = current.nextNode;
+			}
+			try {
+				if(previous == null)
+					first = first.nextNode;
+				else
+					previous.nextNode = current.nextNode;
+			} catch(NullPointerException e) {}
+			System.out.println("\n" + state + " is deleted.");
+		}
+
+		/**
+		 * Method to search through Linked List and find a state.
+		 * @param state - String state 
+		 * @return Node
+		 */
+		public Node find(String state) {
+			Node current = first;
+			while(current != null && state.compareTo(current.name) <= 0) {
+				if(current.name.compareTo(state) < 0)
+					return current;
+				current = current.nextNode;
+			}
+			return null;
+		}
+		
+		/**
+		 * Method goes through Linked Lists and sees if there is more than 1 link, and if so, returns true.
+		 * @return boolean
+		 */
+		public boolean testCollision() {
+			Node current = first;
+			boolean test = false;
+			int counter = 0;
+			while(current != null) {
+				if(counter > 0)
+					test = true;
+				current = current.nextNode;
+				counter++;	
+			}
+			return test;
+		}
+	}
+
+	/**
+	 * Constructor that creates the hash table.
+	 */
+	public HashTable() {
+		arraySize = 101;
+		hashArray = new Link[arraySize];
+		for(int i = 0; i < arraySize; i++) {
+			hashArray[i] = new Link();
+		}
+	}
+
+	/**
+	 * Constructor that sets these variables and creates the object array. 
+	 * @param state - String state
+	 * @param popu - long population 
+	 * @param covidD - long covid deaths
+	 */
+	public HashTable(String state, long popu, long covidD) {
+		this.state = state;
+		this.population = popu;
+		this.covidDeath = covidD;
+		a = new HashTable[max];
+	}
+
+	/**
+	 * Below are multiple getters for state names, populations, and deaths.
+	 * @return
+	 */
+	public String getState() {
+		return this.state; 
+	}
+	public long getPopulation() {
+		return this.population; 
+	}
+	public long getCovidDeath() {
+		return this.covidDeath; 
+	}
+	
+	/**
+	 * Insert excel objects into object array.
+	 * @param arr - HashTable array
+	 * @param nElem - int number of elements
+	 */
+	public void insertArr(HashTable arr[], int nElem) {
+		this.nElems = nElem;
+		this.a = arr;
+	}
+
+	/**
+	 * Method to search the object array for the population and deaths of a state and to call insert(). 
+	 * @param stateIn
+	 */
+	public void insertState(String stateIn) {
+		long population = 0;
+		long deaths = 0;
+		long results[] = search(stateIn);
+		population = results[0];
+		deaths = results[1];
+		insert(stateIn, population, deaths);
+		System.out.println("\n" + stateIn + " is inserted.");
+	}
+
+	/**
+	 * Method to insert a state into the hash table. 
+	 * @param state - String state
+	 * @param population - long population
+	 * @param deaths - long population
+	 */
+	public void insert(String state, long population, long deaths) {
+		Node newNode = new Node(state, population, deaths);
+		int hashVal = hashFunc(state, population, deaths);
+		hashArray[hashVal].insert(newNode);
+	}
+
+	/**
+	 * Method to search the object array for the population and deaths of a state and to call find(). 
+	 * @param searchState - String state
+	 * @return double DR
+	 */
+	public double find(String searchState) {
+		long population = 0;
+		long deaths = 0;
+		long results[] = search(searchState);
+		population = results[0];
+		deaths = results[1];
+		double DR = find(searchState, population, deaths);
+		return DR;
+	}
+
+	/**
+	 * Method to find a state in the hash table. 
+	 * @param state - String state
+	 * @param population - long population
+	 * @param deaths - long population
+	 * @return double DR
+	 */
+	public double find(String state, long population, long deaths) {
+		int hashVal = hashFunc(state, population, deaths);
+		Node theNode = hashArray[hashVal].find(state);
+		System.out.print("\n" + state + " is found at index " + hashVal);
+		return ((double) deaths/population) * 100000;
+	}
+
+	/**
+	 * Method to search the object array for the population and deaths of a state and to call delete(). 
+	 * @param stateName - String state
+	 */
+	public void deleteNode(String stateName) {
+		long population = 0;
+		long deaths = 0;
+		long results[] = search(stateName);
+		population = results[0];
+		deaths = results[1];
+		if(population < 1 || deaths < 1)
+			System.out.println("\n" + stateName + " is not a state.");
+		else
+			delete(stateName, population, deaths);
+	}
+
+	/**
+	 * Method to delete a state in the hash table. 
+	 * @param state - String state
+	 * @param population - long population
+	 * @param deaths - long population
+	 */
+	public void delete(String state, long population, long deaths) {
+		int hashVal = hashFunc(state, population, deaths);
+		hashArray[hashVal].delete(state);
+	}
+
+	/**
+	 * Method to display the hash table.
+	 */
+	public void display() {
+		for(int i = 0; i < arraySize; i++) {
+			System.out.print(i + ". ");
+			hashArray[i].displayList();
+		}
+	}
+
+	/**
+	 * Method to find and print the number of empty cells and collisions. 
+	 */
+	public void printEmptyAndCollisions() {
+		int empty = 0;
+		int collision = 0;
+		boolean test = false;
+		for(int i = 0; i < arraySize; i++) {
+			if(hashArray[i].isEmpty()) 
+				empty++;
+		}
+		for(int j = 0; j < arraySize; j++) {
+			test = hashArray[j].testCollision();
+			if(test)
+				collision++;
+		}
+		System.out.println("\nThere are " + empty + " empty cells and " + collision + " collisions in the hash table.");
+	}
+
+	/**
+	 * Hash function formula.
+	 * @param state - String state
+	 * @param population - long population
+	 * @param deaths - long population
+	 * @return int index of hash
+	 */
+	public int hashFunc(String state, long population, long deaths) {
+		int hashVal = 0;
+		int letter = 0;
+		for(int i = 0; i < state.length(); i++) {
+			letter += state.charAt(i);
+		}
+		hashVal = ((int) (letter + ((population + deaths))) % 101);
+		return hashVal;
+	}
+
+	/**
+	 * Method used to search the object array and return an array of the population and deaths. 
+	 * @param target - String state
+	 * @return - long array with population and deaths
+	 */
+	public long[] search(String target) {
+		int i = 0;
+		long population = 0;
+		long deaths = 0;
+		long[] results = new long[2];
+		while (i < nElems) {
+			if (a[i].getState().equals(target))
+				break;
+			i++;
+			try {
+				population = a[i].getPopulation();
+				deaths = a[i].getCovidDeath();
+			} catch(ArrayIndexOutOfBoundsException e) {}
+		}
+		if (i == nElems) {
+			population = 0;
+			deaths = 0;
+		}
+		results[0] = population;
+		results[1] = deaths;
+		return results;
+	}
+	
 }
--- HashTable_StateProject/src/Project5.java
+++ HashTable_StateProject/src/Project5.java
@@ -1,9 +1,133 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
-public class Project5 {
+/**
+ * COP 3530: Project 5 - Hash Tables
+ * <p>
+ * This program reads in data from an excel sheet and inserts the data into a Binary Search Tree. A menu is then
+ * displayed with options like printing, deleting, and searching.
+ * 
+ * <p>
+ * 
+ * @author Dalton Black
+ * @version 04/13/2021
+ */
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+class Project5 {
+	static Scanner input = new Scanner(System.in);
+
+	/**
+	 * Below is the main function.
+	 * <p>
+	 * It takes user input from an excel file and passes the state name, population, and deaths to hashTable.insert()
+	 * to be hashed. An object array is also created to be used for searching for the population and deaths in later 
+	 * methods. An option menu is then displayed with 6 options - 6 being to quit.
+	 * @param args
+	 */
+	public static void main (String[] args) {
+		int maxSize = 50;
+		HashTable[] arr;
+		arr = new HashTable [maxSize];
+		HashTable hashTable = new HashTable();
+
+		System.out.println("COP3530 Project 5");
+		System.out.println("Instructor: Xudong Liu\n");
+		System.out.println("Hash Table");
+
+		String[] data; //excel data
+		String line = "";
+		String path = null;
+		int nElem = 0;
+
+		System.out.print("Enter the file name: ");
+		path = input.nextLine();
+		System.out.println("");
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(path));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try {	
+			br.readLine();
+			while((line = br.readLine()) != null) {
+				data = line.split(",");
+				String state = data[0];
+				long popu = Integer.parseInt(data[4]);
+				long covidD = Integer.parseInt(data[6]);
+				hashTable.insert(state, popu, covidD);
+				arr[nElem] = new HashTable(state, popu, covidD);
+				nElem++;
+			}
+			hashTable.insertArr(arr, nElem);
+			System.out.println("There were " + nElem + " records read.\n");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+		int userIn = 0;
+		while(userIn != 6) {
+			System.out.print("\n");
+			System.out.println("1) Print hash table");
+			System.out.println("2) Delete a state of a given name");
+			System.out.println("3) Insert a state of a given name");
+			System.out.println("4) Search and print a state and its DR for a given name");
+			System.out.println("5) Print numbs of empty cells and collisions");
+			System.out.println("6) Exit");
+			System.out.print("Please enter your choice: ");
+
+			try {
+				userIn = input.nextInt();
+			} catch (Exception e) {
+				input.next();
+				System.out.println();
+			}
+
+			switch(userIn) {
+			case 1:
+				hashTable.display();
+				break;
+			case 2: 
+				Scanner in = new Scanner(System.in);
+				String stateName;
+				System.out.print("Please enter a state to search for: ");
+				stateName = in.nextLine();
+				hashTable.deleteNode(stateName);
+				break;
+			case 3: 
+				Scanner inState = new Scanner(System.in);
+				String stateIn;
+				System.out.print("Please enter a state to insert: ");
+				stateIn = inState.nextLine();
+				hashTable.insertState(stateIn);
+				break;
+			case 4:
+				Scanner search = new Scanner(System.in);
+				String searchState;
+				System.out.print("Please enter a state to search for: ");
+				searchState = search.nextLine();
+				double DR = hashTable.find(searchState);
+				System.out.printf(" with a DR of %-20.2f\n", DR);
+				break;
+			case 5:
+				System.out.println();
+				hashTable.printEmptyAndCollisions();
+				break;
+			case 6:
+				System.out.println("Have a great day!");
+				break;
+			default:
+				System.out.println("Invalid choice. Please enter 1-6\n");
+				break;
+			}
+		}
+		input.close();
 
 	}
-
 }
